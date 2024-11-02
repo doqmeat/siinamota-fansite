73757f13d9b25b465adeffaa71c0814a0e0c3880
Desteny <desteny282@gmail.com>
2024-11-01 23:36:41 -0400

    renamed folders

---
+++ fansite/scans/script.js
@@ -0,0 +1,35 @@
+//thank u to stackoverflow always https://stackoverflow.com/questions/26672104/get-attributes-value-for-multiple-class
+
+var scanDiv = document.getElementById("picture-popup");
+var scanPic = document.getElementsByClassName("scans");
+var popupPic = document.getElementById("picture-img");
+var captionWin = document.getElementById("caption");
+
+for (var i = 0; i < scanPic.length; i++) {
+  var scanPicClick = scanPic[i];
+  console.log(scanPicClick);
+  scanPicClick.onclick = function (windowpopup) {
+    scanDiv.style.display = "block";
+    popupPic.src = this.src;
+    popupPic.alt = this.alt;
+    captionWin.innerText = this.alt;
+  };
+}
+
+function toggle(cds) {
+  var i;
+  var popUp = document.getElementById(cds);
+  var notpopUp = document.getElementsByClassName("pop-up");
+  for (i = 0; i < notpopUp.length; i++) {
+    notpopUp[i].style.display = "none";
+  }
+
+  if (popUp.style.display == "none") {
+    popUp.style.display = "block";
+  }
+}
+
+function toggleClose(ids) {
+  var popUp = document.getElementById(ids);
+  popUp.style.display = "none";
+}
