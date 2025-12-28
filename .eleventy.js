dde216824faab49fa65ccb035566630d957f0d61
doqmeat <desteny282@gmail.com>
2025-12-28 00:11:52 -0400

    fixing 11ty template

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,26 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setServerPassthroughCopyBehavior("copy");
-	eleventyConfig.addPassthroughCopy("files");
+
+	// folders and files that eleventy won't process
+	eleventyConfig.addPassthroughCopy("_files");
 	eleventyConfig.addPassthroughCopy("fanlisting");
 	eleventyConfig.addPassthroughCopy("bot");
 	eleventyConfig.addPassthroughCopy("rss.xml");
+
 	// This will stop the default behaviour of foo.html being turned into foo/index.html
 	eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+	// default layout: main.html
+	eleventyConfig.addGlobalData("layout", "main.html");
+	// default date format
+	eleventyConfig.addGlobalData("date", "git Last Modified");
+
+	// shortcode for year {% year %} --- not using it rn
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	// custom page.date ----- {{ page.date | postDate }}
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toFormat("dd.MMM.yy");
+	});
 };
